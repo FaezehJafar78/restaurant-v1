81cf7b85ac1d48b0b3436647e3e62969ce42d820
faezeh <faezehj487@gmail.com>
2021-11-18 20:25:41 +0330

    add banner

---
+++ src/mainPage/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import React, { fragment } from 'react';
+import stylesheet from './Banner.module.css';
+
+const Banner = () => {
+return (
+  <fragment>
+    <section className={stylesheet.banner}>jk</section>
+  </fragment>
+);
+};
+export default Banner;
--- src/mainPage/layout/Main.js
+++ src/mainPage/layout/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import stylesheet from './Main.module.css';
+import Banner from '../components/Banner/Banner';
 
 export const Main = () => {
     return (
       <div>
-        <p className={stylesheet.main}>main</p>
+        <p className={stylesheet.main} />
+        <Banner />
       </div>
     );
 };
